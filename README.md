1116413950400dd5b9bc951c38d86664523bc99e
gerritonagoodday <gerrit@hoekstra.co.uk>
2017-12-06 12:20:37 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# pdfreplacetxt
+
+Use this utility to replace text in a PDF file without having to edit the file with a bespoke PDF editor. This can only do basic replacements using regular expressions such as you might use on a text file using the 'sed' command e.g.
+
+	sed -e 's/[search pattern]/[replacement pattern/' -i textfile
+
+The equivalent for a PDF file is:
+
+	pdfreplacetxt file.pdf "search pattern" "replacement pattern"
+
+# Watch out for:
+
+- Do not assume that line breaks inside a PDF file are going to be where they appear when a PDF viewer displays a PDF file. It is therefore important not to use open-ended regexes such as \"*. etc\" for a search pattern. Generally, be very specific what you are looking for to replace. Just like normal 'SED', this 'SED' does not span across lines either.
+- If the replacement string is much longer than the searched-for string, the resulting document may loose some of its formatting.
+- If the letters in a word in the searched-for string are written in different fonts or sizes or offsets, the chances are pretty slim that the searched-for string will be found.
+- In rare cases, the text to be removed is encapsulated inside a font in the PDF file, so can't readily be identified in order for it to be removed using this process.
+- Sometimes a PDF file contains an entire bitmap image and no text. This is especially the case for PDF documents that were created from scannned paper documents. This utility will not help in such cases.
+- Take care not to specify a PDF control term to be replaced, as this can potentially corrupt your entire PDF file.
+
+# Prerequisites:
+
+You need to have the PDF Toolkit installed, a.k.a. "pdftk". Depending on your Linux distro, use one of the following installation commands depending on what version of Linux you have:
+
+    yum install pdftk
+    apt-get install pdftk
+    emerge pdftk
+
+# Usage:
+
+    pdfreplacetxt ebook.pdf "09 October" "10 October"
+
+# Installation:
+
+Copy these files into your /usr/local/bin directory
+
+    $ sudo cp pdfreplacetxt* /usr/local/bin
+
+# Example:
+
+To change the face-value of a PDF cheque (that a "check" to you lot in America) from $100 to $1000, do could do this (but you shouldn't):
+
+    pdfreplacetxt cheque.pdf "100\.00" "1000\.00"
+    pdfreplacetxt cheque.pdf "HUNDRED" "THOUSAND"
+
+BTW, don't do this, it's called fraud. I am using this as an example:
+
+- To illustrate that most PDF documents are not secured and can easily be adulterated.
+- Because cheques should not be used as a modern payment method anyway. Cheques and the people who use them are both stupid. And the banks that still advocate cheques are greedy.
+
+To become a Big-Time criminal and apply this to all the $100 cheques in a directory tree, do this to secure your place in jail next to Bernie Madoff:
+
+    find . -name "*.pdf" -exec pdfreplacetxt {} "100\.00" "1000\.00" \; -print
+    find . -name "*.pdf" -exec pdfreplacetxt {} "HUNDRED" "THOUSAND" \; -print
+
+# Author:
+
+Gerrit Hoekstra. You can contact me via https://github.com/gerritonagoodday
+
+# Environmental Notice:
+
+This work was created from 100%-recycled electrons. No animals were hurt during the production of this work, except when I forgot to feed my cats that one time. The cats and I are on speaking terms again.
---
+++ pdfreplacetxt.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+
+PROGNAME=${0##*/}
+STARTTIME=$(date +%s)
+
+function Usage {
+  printf "
+Usage:
+  ${0##*/} filepath.pdf \"search pattern\" \"replacement pattern\"
+
+Function:
+  Use this utility to replace text in a PDF file without having to edit the
+  file with a PDF editor. This can only do basic replacements using regular
+  expressions such as you might use on a text file using the 'sed' command
+  e.g. sed -e 's/[search pattern]/[replacement pattern/' -i textfile
+
+  Do not assume that line breaks inside a PDF file are going to be where they
+  appear when a PDF viewer displays a PDF file. It is therefore important not
+  to use open-ended regexes such as \"*. etc\" for a search patternn.
+
+Example:
+	pdfsed ebook.pdf \"09 October\" \"10 October\"
+
+Author:
+  Gerrit Hoekstra. You can contact me via https://github.com/gerritonagoodday
+"
+  exit 1
+}
+
+function die {
+  if [[ -z $1 ]]; then
+    printf "$(tput setaf 9)failed.\nExiting...\n$(tput sgr 0)"
+  else
+    printf "$(tput setaf 9)*** $1 ***\nExiting...\n$(tput sgr 0)"
+  fi
+  exit 1
+}
+function warn {
+  printf "$(tput setaf 3)Warning: $1\n$(tput sgr 0)"
+}
+function info {
+  printf "$(tput setaf 10)$1...\n$(tput sgr 0)"
+}
+function doneit {
+  if [[ -n $1 ]]; then
+    printf "$(tput setaf 12)$1, done\n$(tput sgr 0)"
+  else
+    printf "$(tput setaf 12)done\n$(tput sgr 0)"
+  fi
+}
+
+PDFFILE="${1}"
+[[ -z $PDFFILE ]] && Usage
+SEARCHPATTERN="${2}"
+[[ -z $SEARCHPATTERN ]] && Usage
+REPLACEMENTPATTERN="${3}"
+[[ -z $REPLACEMENTPATTERN ]] && Usage
+
+# Temp work files
+TMPFILE1=$(mktemp "/tmp/tmp.${PROGNAME}.$$.XXXXXX")
+#TMPFILE2=$(mktemp "/tmp/tmp.${PROGNAME}.$$.XXXXXX")
+
+#============================================================================#
+# Set traps and signal BEGIN and END
+# Need logging to work for this
+#============================================================================#
+function cleanup {
+  rm "${TMPFILE1}" 2>/dev/null
+  #rm "${TMPFILE2}" 2>/dev/null
+  ENDTIME=$(date +%s)
+  elapsedseconds=$((ENDTIME-STARTTIME))
+  s=$((elapsedseconds % 60))
+  m=$(((elapsedseconds / 60) % 60))
+  h=$(((elapsedseconds / 60 / 60) % 24))
+  duration=$(printf "Duration (h:m:s): %02d:%02d:%02d" $h $m $s)
+  doneit "${duration}"
+  exit
+}
+for sig in KILL TERM INT EXIT; do trap 'cleanup $sig' "$sig" ; done
+
+info "Checking environment"
+PDFTK=`which pdftk 2>/dev/null`
+[[ -z "$PDFTK" ]] && die "pdftk does not appear to be installed."
+info "Checking $PDFFILE"
+[[ ! -f $PDFFILE ]] && die "$PDFFILE does not exist"
+filetype=$(file -b "$PDFFILE")
+if [[ $filetype =~ ^PDF ]]; then
+  info "$PDFFILE is a PDF file"
+else
+  die "File $PDFFILE does not appear to be a PDF file."
+fi
+
+info "Uncompressing $PDFFILE"
+pdftk "$PDFFILE" output $TMPFILE1 uncompress  > /dev/null 2>&1
+retcode=$?
+[[ $retcode -ne 0 ]] && die "pdftk returned error code $retcode"
+
+info "Replacing '$SEARCHPATTERN' with '$REPLACEMENTPATTERN' in $PDFFILE"
+# Fix patterns with deal with PDF formatting codes
+SEARCHPATTERN=$(echo $SEARCHPATTERN | sed -e 's/ /.*/g')
+# Deal with patterns that contains actual speech marks
+SEARCHPATTERN=$(echo $SEARCHPATTERN | sed -e "s|'|\x27|g" | sed -e 's/"/\x22/g')
+REPLACEMENTPATTERN=$(echo $REPLACEMENTPATTERN | sed -e "s|'|\x27|g" | sed -e 's/"/\x22/g')
+sed -e "s|${SEARCHPATTERN}|${REPLACEMENTPATTERN}|g" -i $TMPFILE1
+
+info "Re-Compressing $PDFFILE"
+pdftk $TMPFILE1 output "$PDFFILE" compress  > /dev/null 2>&1
+retcode=$?
+[[ $retcode -ne 0 ]] && die "pdftk returned error code $retcode"
+
+
